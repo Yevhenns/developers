fc4a1dd6ce2c000453a17ea972392d8ad2002dba
Yevhenns <gifestum@gmail.com>
2024-03-19 21:30:37 +0200

    upload styles

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -3,12 +3,17 @@ import css from './Input.module.scss';
 
 export type InputProps = {
   error?: string;
+  type?: 'file';
 } & HTMLProps<HTMLInputElement>;
 
-export const Input: FC<InputProps> = forwardRef(({ error, ...props }, ref) => {
+export const Input: FC<InputProps> = forwardRef(({ type, error, ...props }, ref) => {
   return (
     <div className={css.fieldset}>
-      <input className={css.input} autoComplete="true" {...props} ref={ref} />
+      {type === 'file' ? (
+        <input className={css.upload} type={type} {...props} ref={ref} />
+      ) : (
+        <input className={css.input} autoComplete="true" {...props} ref={ref} />
+      )}
       <label htmlFor={props.htmlFor}>{props.label}</label>
       {error && <span className={css.errorMessage}>{error}</span>}
     </div>
